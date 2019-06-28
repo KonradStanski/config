180dc5f962ba36db950fd6916bf3e8c56e0c930b
Konrad Staniszewski <kstanisz@ualberta.ca>
2019-06-28 16:15:25 -0400

    fixed typo in mint.sh

--- setup/mint/mint_install.sh
+++ setup/mint/mint_install.sh
@@ -24,7 +24,7 @@ sudo apt-get install spotify-client -y
 
 # install sublime
 echo ===================================================================================================
-echo spotify
+echo sublime
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt install sublime-text -y
