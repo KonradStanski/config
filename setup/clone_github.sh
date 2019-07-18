7ac6640dbed87f889d7fb194a5fa8586acc451e3
konradstanski <kstanisz@ualberta.ca>
2019-07-18 15:47:04 -0400

    added tmuxa for attaching to session easily

--- setup/clone_github.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-declare -a array=("config" "resume" "Konradstaniszewski.github.io")
-
-for i in "${array[@]}"
-do
-	git clone https://github.com/konradstanski/"$i".git ~/code/"$i"
-done
--- setup/fedora/optional_programs.sh
+++
@@ -1,5 +0,0 @@
-# Intsall vscode on fedora
-sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-dnf check-update
-sudo dnf install code
