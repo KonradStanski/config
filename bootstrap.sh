793329af652d9e398e07891d7d0930a8f675765b
Konrad <konrad.a.staniszewski@gmail.com>
2022-12-09 15:16:56 -0800

    update bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,18 +1,31 @@
 #!/bin/bash
 
+#####################################################
+#             KONRAD'S BOOTSRAP SCRIPT              #
+#####################################################
+# This Script should be idempotent, meaning it can be run multiple times without causing problems
+
+# Check if the user is running the script with root privileges
+if [[ $EUID -ne 0 ]]; then
+  echo "This script must be run with root privileges (sudo)"
+  exit 1
+fi
+
 install_linux() {
    # commands to install software on Linux go here
    echo "Bootstrapping linux config"
-   install/linux/install.sh
-   cd stow
+   cd install/linux
+   ./install.sh
+   cd ../../stow
    make stow dir=linux
 }
 
 install_macos() {
    # commands to install software on macOS go here
    echo "Bootstrapping macos config"
-   sudo install/macos/install.sh
-   cd stow
+   cd install/macos
+   ./install.sh
+   cd ../../stow
    make -f stow/make stow dir=macos
 }
 
