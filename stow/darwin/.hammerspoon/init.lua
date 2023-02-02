08ce176ce83d9cb16a605d8ac68e23c59c84cdb6
Konrad Staniszewski <konrad.a.staniszewski@gmail.com>
2023-02-01 16:43:34 -0800

    setup hammerspoon (spaces need work)

---
+++ stow/darwin/.hammerspoon/init.lua
@@ -0,0 +1,43 @@
+hs.console.clearConsole()
+hs.alert.show("Config loaded")
+
+
+hs.hotkey.bind({ "cmd", "alt", "ctrl" }, "R", function()
+  hs.reload()
+end)
+
+-- Open iterm window
+hs.hotkey.bind({ "cmd", "ctrl" }, "T", function()
+  hs.osascript.applescript([[
+    tell application "iTerm2"
+      set newWindow to (create window with default profile)
+    end tell
+  ]])
+end)
+
+-- Open new chrome window
+hs.hotkey.bind({ "cmd", "ctrl" }, "G", function()
+  hs.osascript.applescript([[
+    tell application "Google Chrome"
+      make new window
+      activate
+    end tell
+  ]])
+end)
+
+-- Open new finder window
+hs.hotkey.bind({ "cmd" }, "E", function()
+  hs.osascript.applescript([[
+    set myTarget to ((path to documents folder) as text)
+    tell application "Finder" 
+      set myW to make new Finder window
+      set target of myW to myTarget
+      activate
+    end tell
+  ]])
+end)
+
+require("chrome")
+require("spaces")
+
+print("************CONFIG LOADED************")
---
+++ stow/darwin/.hammerspoon/spaces.lua
@@ -0,0 +1,7 @@
+function MoveWindowToSpace(sp)
+
+end
+
+hs.hotkey.bind({ "cmd", "shift" }, '1', function() MoveWindowToSpace(1) end)
+hs.hotkey.bind({ "cmd", "shift" }, '2', function() MoveWindowToSpace(2) end)
+hs.hotkey.bind({ "cmd", "shift" }, '3', function() MoveWindowToSpace(3) end)
