836bb9d6876907b4825f687979ab999ef3118bcb
Konrad <konrad.a.staniszewski@gmail.com>
2023-02-05 01:28:30 -0800

    Redo terminal choice and add casks

--- stow/common/bin/TerminalChoice.py
+++ stow/common/bin/TerminalChoice.py
@@ -3,88 +3,94 @@ import sys
 import tty
 import termios
 
+
 class CommandChooser():
-   # Constructor
-   def __init__(self):
-      # Command to get the list of options
-      self.get_options_command = ['echo', 'Number Option\n1: Option1\n2: Option2\n3: Option3']
-      # Command to choose an option (ie. attach to a container)
-      self.choose_option_command = ['echo', 'You chose option']
-      # Wether to ignore the first few lines (usually a header)
-      self.ignore_first_n_lines = 1
-      # Index of the option id in the option string (the id is used in the choose_command)
-      self.option_id_index = 0
-      # Splitting character for the option string
-      self.option_split_char = ':'
-      # Shell true or false (used for commands with weird escape sequences that need to be a single string)
-      self.shell = False
+    # Constructor
+    def __init__(self):
+        # Command to get the list of options
+        self.get_options_command = [
+            'echo', 'Number Option\n1: Option1\n2: Option2\n3: Option3']
+        # Command to choose an option (ie. attach to a container)
+        self.choose_option_command = ['echo', 'You chose option']
+        # Wether to ignore the first few lines (usually a header)
+        self.ignore_first_n_lines = 1
+        # Index of the option id in the option string (the id is used in the choose_command)
+        self.option_id_index = 0
+        # Splitting character for the option string
+        self.option_split_char = ':'
+        # Shell true or false (used for commands with weird escape sequences that need to be a single string)
+        self.shell = False
 
+    # Get the list of options
 
-   # Get the list of options
-   def get_options(self):
-      options = subprocess.run(self.get_options_command, shell=self.shell, capture_output=True).stdout.strip().decode('utf-8')
-      options = options.split('\n')
-      return options
+    def get_options(self):
+        options = subprocess.run(self.get_options_command, shell=self.shell,
+                                 capture_output=True).stdout.strip().decode('utf-8')
+        options = options.split('\n')
+        return options
 
+    def print_options(self, options, selected_index):
+        width, _ = subprocess.run(['stty', 'size'], capture_output=True).stdout.decode('utf-8').split()
+        width = int(width)
+        # truncate the options to the width of the terminal
+        options = [option[:width] for option in options]
+        for i, option in enumerate(options):
+            if i == selected_index:
+                print(f"\033[1m> {option}\033[0m")
+            else:
+                print(f"  {option}")
 
-   def print_options(self, options, selected_index):
-    for i, option in enumerate(options):
-        if i == selected_index:
-            print(f"\033[1m> {option}\033[0m")
-        else:
-            print(f"  {option}")
+    # Read user input using getch()
+    def getch(self):
+        # Save the current terminal settings
+        old_settings = termios.tcgetattr(sys.stdin)
+        try:
+            # Set the terminal to raw mode
+            tty.setraw(sys.stdin.fileno())
+            # Read a single character from the user
+            ch = sys.stdin.read(1)
+            # Check for escape sequences
+            if ch == "\x1b":
+                # Read the next two characters
+                ch = ch + sys.stdin.read(2)
+                if ch == "\x1b":
+                    # if it was just an escape key, exit
+                    sys.exit(0)
+        finally:
+            # Restore the terminal settings
+            termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
+        return ch
 
-   # Read user input using getch()
-   def getch(self):
-      # Save the current terminal settings
-      old_settings = termios.tcgetattr(sys.stdin)
-      try:
-         # Set the terminal to raw mode
-         tty.setraw(sys.stdin.fileno())
-         # Read a single character from the user
-         ch = sys.stdin.read(1)
-         # Check for escape sequences
-         if ch == "\x1b":
-               # Read the next two characters
-               ch = ch + sys.stdin.read(2)
-               if ch == "\x1b":
-                  # if it was just an escape key, exit
-                  sys.exit(0)
-      finally:
-         # Restore the terminal settings
-         termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
-      return ch
-   
-   def run(self):
-      options = self.get_options()
-      selected_index = self.ignore_first_n_lines
-      self.print_options(options, selected_index)
+    def run(self):
+        options = self.get_options()
+        selected_index = self.ignore_first_n_lines
+        self.print_options(options, selected_index)
 
-      while True:
-         # Move the cursor back to the beginning of the line
-         sys.stdout.write(f"\033[{len(options)}F")
-         self.print_options(options, selected_index)
+        while True:
+            # Move the cursor back to the beginning of the line
+            sys.stdout.write(f"\033[{len(options)}F")
+            self.print_options(options, selected_index)
 
-         # Read a single character from the user
-         ch = self.getch()
+            # Read a single character from the user
+            ch = self.getch()
 
-         # Handle the arrow keys
-         if ch == "\x1b[A" or ch == "k":
-            # Up arrow
-            selected_index = max(self.ignore_first_n_lines, selected_index - 1)
-         elif ch == "\x1b[B" or ch == "j":
-            # Down arrow
-            selected_index = min(len(options) - 1, selected_index + 1)
-         elif ch == "\r":
-            # Enter key
-            break
-         elif ch == "q":
-            sys.exit(0)
+            # Handle the arrow keys
+            if ch == "\x1b[A" or ch == "k":
+                # Up arrow
+                selected_index = max(
+                    self.ignore_first_n_lines, selected_index - 1)
+            elif ch == "\x1b[B" or ch == "j":
+                # Down arrow
+                selected_index = min(len(options) - 1, selected_index + 1)
+            elif ch == "\r":
+                # Enter key
+                break
+            elif ch == "q":
+                sys.exit(0)
 
-      option_id = options[selected_index].split(self.option_split_char)[0]
-      # connect to the session using the number
-      subprocess.run(self.choose_option_command + [option_id])
-
+        option_id = options[selected_index].split(self.option_split_char)[0]
+        # connect to the session using the number
+        subprocess.run(self.choose_option_command + [option_id])
 
 
 # A NIFTY INTERPRETATION
