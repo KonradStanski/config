fd3cb15fdc0fde4fb696c0980448b2dbeebefc39
Konrad Staniszewski <konrad.a.staniszewski@gmail.com>
2023-02-07 00:52:05 -0800

    update a few linux things

--- stow/linux/.config/zsh/platform.zsh
+++ stow/linux/.config/zsh/platform.zsh
@@ -1,2 +1,5 @@
 # Platform specific zsh stuff goes in here.
 # This is linux specific zsh stuff.
+
+# works on ubuntu with apt install fzf
+source /usr/share/doc/fzf/examples/key-bindings.zsh # fzf keybindings
