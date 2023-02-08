eec2a9e1392055b15bd59b859ec4a6f03b4a0c7c
Konrad <konrad.a.staniszewski@gmail.com>
2023-02-08 09:17:08 -0800

    add nix for fzf bindings

--- stow/linux/.config/zsh/platform.zsh
+++ stow/linux/.config/zsh/platform.zsh
@@ -1,5 +1,9 @@
 # Platform specific zsh stuff goes in here.
 # This is linux specific zsh stuff.
 
-# works on ubuntu with apt install fzf
-source /usr/share/doc/fzf/examples/key-bindings.zsh # fzf keybindings
+# enable fzf keybindings either from apt install or nixpkgs
+if [ -f /usr/share/doc/fzf/examples/key-bindings.zsh ] ; then
+   source /usr/share/doc/fzf/examples/key-bindings.zsh
+elif [ -f ~/.nix-profile/share/fzf/key-bindings.zsh ] ; then
+   source ~/.nix-profile/share/fzf/key-bindings.zsh
+fi
