352cda159c3bd94a63cdb21c4b5c5277c603030a
Konrad <konrad.a.staniszewski@gmail.com>
2024-04-04 22:58:00 -0700

    add phoenix changes

--- stow/darwin/.config/phoenix/index.ts
+++ stow/darwin/.config/phoenix/index.ts
@@ -2,7 +2,9 @@
 Phoenix.set({
   openAtLogin: true,
 });
-Phoenix.log("#################################CONFIG RELOADED#################################");
+Phoenix.log(
+  "#################################CONFIG RELOADED#################################"
+);
 
 Key.on("o", ["ctrl", "cmd"], () => {
   toggleFocus("Obsidian");
@@ -15,6 +17,42 @@ Key.on("c", ["ctrl", "cmd"], () => {
 Key.on("k", ["ctrl", "cmd"], () => {
   toggleFocus("kitty");
 });
+
+// this is not quite ready as the space hashes don't quite line up with their order
+// Key.on("1", ["shift", "cmd"], () => {
+//   moveFocusedWindowToSpace(1);
+// });
+
+// Key.on("2", ["shift", "cmd"], () => {
+//   moveFocusedWindowToSpace(2);
+// });
+
+// Key.on("3", ["shift", "cmd"], () => {
+//   moveFocusedWindowToSpace(3);
+// });
+
+// Key.on("4", ["shift", "cmd"], () => {
+//   moveFocusedWindowToSpace(4);
+// });
+
+// Key.on("5", ["shift", "cmd"], () => {
+//   moveFocusedWindowToSpace(5);
+// });
+
+// Key.on("6", ["shift", "cmd"], () => {
+//   moveFocusedWindowToSpace(6);
+// });
+
+function moveFocusedWindowToSpace(spaceIndex: number) {
+  let window = Window.focused();
+  if (window) {
+    let spaces = Space.all();
+    let space = spaces.find((space) => space.hash() === spaceIndex);
+    if (space) {
+      space.moveWindows([window]);
+    }
+  }
+}
 
 function toggleFocus(appName: string): App | undefined {
   let app = App.get(appName);
--- stow/darwin/.config/phoenix/phoenix.js
+++ stow/darwin/.config/phoenix/phoenix.js
@@ -1,10 +1,20 @@
 // /Users/konrad/.config/phoenix/index.ts
+var moveFocusedWindowToSpace = function(spaceIndex) {
+  let window = Window.focused();
+  if (window) {
+    let spaces = Space.all();
+    let space = spaces.find((space2) => space2.hash() === spaceIndex);
+    if (space) {
+      space.moveWindows([window]);
+    }
+  }
+};
 var toggleFocus = function(appName) {
   let app = App.get(appName);
   if (app) {
     if (app.isActive()) {
       app.hide();
-      return;
+      return app;
     }
     let window = app.mainWindow();
     let space = Space.active();
@@ -13,7 +23,7 @@ var toggleFocus = function(appName) {
     }
     app.focus();
   } else {
-    App.launch(appName, { focus: true });
+    return App.launch(appName, { focus: true });
   }
 };
 Phoenix.set({
@@ -28,4 +38,22 @@ Key.on("c", ["ctrl", "cmd"], () => {
 });
 Key.on("k", ["ctrl", "cmd"], () => {
   toggleFocus("kitty");
+});
+Key.on("1", ["shift", "cmd"], () => {
+  moveFocusedWindowToSpace(1);
+});
+Key.on("2", ["shift", "cmd"], () => {
+  moveFocusedWindowToSpace(2);
+});
+Key.on("3", ["shift", "cmd"], () => {
+  moveFocusedWindowToSpace(3);
+});
+Key.on("4", ["shift", "cmd"], () => {
+  moveFocusedWindowToSpace(4);
+});
+Key.on("5", ["shift", "cmd"], () => {
+  moveFocusedWindowToSpace(5);
+});
+Key.on("6", ["shift", "cmd"], () => {
+  moveFocusedWindowToSpace(6);
 });
