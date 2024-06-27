e018a811a115f6e2d35c22c5cc106cdf6625050a
Konrad <konrad.a.staniszewski@gmail.com>
2024-06-27 14:06:02 -0700

    Add command to bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,6 @@
 # download git repo and unzip it
+# copy-pastable command for running this script anywhere:
+# curl -fsSL https://raw.githubusercontent.com/konradstanski/config/master/bootstrap.sh | bash
 curl -sL https://github.com/konradstanski/config/archive/master.tar.gz | tar xz
 cd config-master
 ./install.sh
