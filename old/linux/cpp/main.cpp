d2e84fa3a4798c15216c74e048bbd73227ff20b3
Konrad <konrad.a.staniszewski@gmail.com>
2024-07-25 11:17:23 -0700

    delete old

--- old/linux/cpp/main.cpp
+++
@@ -1,6 +0,0 @@
-#include <bits/stdc++.h> // this imports entire stl, good for comp, inneficient.
-
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-}
--- old/linux/scripts/monitor_changes.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-# This Script watches the current directory for changes
-# will run the code in the while loop if something changes
-
-while inotifywait -q -r -e modify ./; do
-	pdflatex resume.tex
-done
