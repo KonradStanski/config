8e332f03ba1648bfb7a8299d798185867361ba01
Konrad <konrad.a.staniszewski@gmail.com>
2025-12-03 11:09:52 -0800

    add yazi to install and theme

--- stow/darwin/.hammerspoon/space_names.lua
+++ stow/darwin/.hammerspoon/space_names.lua
@@ -3,9 +3,9 @@ function flashDesktopNameOrID()
     local screen = hs.screen.primaryScreen()
     local currentSpaceID = hs.spaces.activeSpaceOnScreen(screen)
     local spaceName = hs.settings.get("spaceNames_" .. tostring(currentSpaceID)) or "Desktop ID: " .. tostring(currentSpaceID)
-    
+
     -- Show the alert with the space name or ID for a short duration
-    hs.alert.show(spaceName, { atScreenEdge = 2 }, screen, 0.5)
+    hs.alert.show(spaceName, screen, 1)
 end
 
 -- Function to rename the current desktop
@@ -13,7 +13,7 @@ function renameCurrentDesktop()
     local screen = hs.screen.primaryScreen()
     local currentSpaceID = hs.spaces.activeSpaceOnScreen(screen)
     local currentName = hs.settings.get("spaceNames_" .. tostring(currentSpaceID)) or "Desktop ID: " .. tostring(currentSpaceID)
-    
+
     -- Prompt for a new name
     local button, newSpaceName = hs.dialog.textPrompt("Rename Desktop", "Enter a new name for this space:", currentName, "OK", "Cancel")
     if button == "OK" and newSpaceName ~= "" then
@@ -23,12 +23,12 @@ function renameCurrentDesktop()
 end
 
 -- Bind the space change event to flashDesktopNameOrID function
-hs.spaces.watcher.new(function()
-    flashDesktopNameOrID()
-end):start()
+-- hs.spaces.watcher.new(function()
+--     flashDesktopNameOrID()
+-- end):start()
 
 -- Hotkey to rename the current desktop (Ctrl+Alt+R)
-hs.hotkey.bind({"ctrl", "alt"}, "R", function()
-    renameCurrentDesktop()
-end)
+-- hs.hotkey.bind({"ctrl", "alt"}, "R", function()
+--     renameCurrentDesktop()
+-- end)
 
